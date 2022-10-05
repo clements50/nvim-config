5148ad8d4010fa3dc6f931126a6b3ba35f2291e8
Cody Clements <subliminalverses2@gmail.com>
2022-10-05 08:51:36 -0500

    added indent lines

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -9,3 +9,8 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
+
+--indent-blankline
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -1,19 +1,19 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- My plugins here
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	-- My plugins here
 
 	--color theme
 	use("navarasu/onedark.nvim")
@@ -47,27 +47,29 @@ return require('packer').startup(function(use)
 
 	--dap
 	use("mfussenegger/nvim-dap")
-    use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-    use { "mxsdev/nvim-dap-vscode-js", requires = {"mfussenegger/nvim-dap"} }
+	use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
+	use({ "mxsdev/nvim-dap-vscode-js", requires = { "mfussenegger/nvim-dap" } })
 
-    --vscode dap debugger
-    use {
-  "microsoft/vscode-js-debug",
-  opt = true,
-  run = "npm install --legacy-peer-deps && npm run compile" 
-}
+	--vscode dap debugger
+	use({
+		"microsoft/vscode-js-debug",
+		opt = true,
+		run = "npm install --legacy-peer-deps && npm run compile",
+	})
 
-    --autotag and autotags
-    use('windwp/nvim-ts-autotag')
-use {
-	"windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-}
+	--autotag and autotags
+	use("windwp/nvim-ts-autotag")
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
---cmp
-use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
+	--cmp
+	use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
 	use({ "hrsh7th/cmp-buffer" }) -- buffer completions
-	use({ "hrsh7th/cmp-path"}) -- path completions
+	use({ "hrsh7th/cmp-path" }) -- path completions
 	use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
 	use({ "hrsh7th/cmp-nvim-lsp" })
 	use({ "hrsh7th/cmp-nvim-lua" })
@@ -76,10 +78,15 @@ use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
 	use({ "L3MON4D3/LuaSnip" }) --snippet engine
 	use({ "rafamadriz/friendly-snippets" }) --
 
-    --null ls
-   use('jose-elias-alvarez/null-ls.nvim')
+	--null ls
+	use("jose-elias-alvarez/null-ls.nvim")
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	--indent-blankline
+	require("packer").startup(function()
+		use("lukas-reineke/indent-blankline.nvim")
+	end)
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
