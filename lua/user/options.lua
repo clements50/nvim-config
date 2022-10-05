be634bf5b83e4e842a00a3fb93052b91a86bdf06
Cody Clements <subliminalverses2@gmail.com>
2022-10-05 09:05:58 -0500

    tweak indent line options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -12,5 +12,5 @@ vim.opt.wrap = false
 
 --indent-blankline
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+--vim.opt.listchars:append("space:⋅")
+--vim.opt.listchars:append("eol:↴")
