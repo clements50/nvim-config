ad7811259cd6eb9bc564e195fdda73aabcb43bf8
Cody <subliminalverses2@gmail.com>
2022-11-14 15:19:45 -0600

    add git signs

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # nvim-config
+
     git clone git@github.com:clements50/nvim-config.git ~/.config/nvim
 
 # uninstall linux/mac
+
     rm -rf ~/.config/nvim
     rm -rf ~/.local/share/nvim
     rm -rf ~/.cache/nvim
 
 # uninstall windows
+
     rd -r ~\AppData\Local\nvim
     rd -r ~\AppData\Local\nvim-data
-
--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -1,23 +1,23 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 
-    --theme
-	use "navarasu/onedark.nvim"
-     
-    --tresitter
+	--theme
+	use("navarasu/onedark.nvim")
+
+	--tresitter
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = function()
@@ -81,10 +81,17 @@ return require('packer').startup(function(use)
 	use("jose-elias-alvarez/null-ls.nvim")
 
 	--indent-blankline
-		use("lukas-reineke/indent-blankline.nvim")
+	use("lukas-reineke/indent-blankline.nvim")
 
+	--gitsigns
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
